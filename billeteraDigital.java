7f7b66625a1c221c6390b1c966ba7654c3feaf23
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-08-19 14:30:28 -0500

    Agrego mi nombre

--- billeteraDigital.java
+++ billeteraDigital.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author hp
+ /*
+ * @IlanNestorAngelesRodriguez
  */
 import java.util.Scanner;
 public class billeteraDigital {
