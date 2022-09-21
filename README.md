a63fac404418510929bc4d623c42f39fe88a05fa
Ilan Néstor Angeles Rodriguez <ilanangelesrodriguez@gmail.com>
2022-09-21 15:26:27 -0500

    Nuevo commit para el proyecto

--- README.md
+++
@@ -1,2 +0,0 @@
-# Wallet
-This is wallet for EPISIcoin token, is elaborated in pseudocode and java.
--- src/main/java/billeteraDigital.java
+++ src/main/java/billeteraDigital.java
@@ -1,29 +1,56 @@
-
- /*
- * @IlanNestorAngelesRodriguez
+/*
+ * @ilanangelesrodriguez
  */
 import java.util.Scanner;
 public class billeteraDigital {
     public static void main(String[] args) {
-        int id, password;
-        double episicoin;
+        int  id,password, episicoin;
         episicoin=100;
+        /*Direcciones*/
+        int [] direccion=new int[5];
+        direccion[0]=12345678;
+        direccion[1]=23456789;
+        direccion[2]=34567891;
+        direccion[3]=45678912;
+        direccion[4]=56789123;
+        /*Contraseñas*/
+        int [] contraseña=new int[5];
+        contraseña[0]=1234;
+        contraseña[1]=2345;
+        contraseña[2]=3456;
+        contraseña[3]=5678;
+        contraseña[4]=6789;
+
         System.out.println("-----------------------------------");
-	System.out.println("|=== Red principal de Ethereum ===|");
-	System.out.println("-----------------------------------");
+	    System.out.println("|=== Red principal de Ethereum ===|");
+	    System.out.println("-----------------------------------");
         System.out.println("=== Bienvenido a la EPISIwallet ===");
         Scanner teclado = new Scanner(System.in);
         System.out.print("Introduce tu direccion:");
-	id=teclado.nextInt();
-	System.out.print("Introduce tu clave:");
-        password=teclado.nextInt();
-        wait(2);
-        iniciarsesion();
-        wait(2);
-        menu();
+        id=teclado.nextInt();
+        int elemento=busqueda(direccion,id);
+        if (elemento == -1){
+            System.out.println("No existe");
+        }else {
+            System.out.println("Bienvenido");
+            System.out.print("Introduce tu clave:");
+            password=teclado.nextInt();
+            wait(2);
+            iniciarsesion();
+            wait(2);
+            menu(episicoin);
+        }
     }
     public static void iniciarsesion(){
-        System.out.println("Iniciando Sesion.............."); 
+        System.out.println("Iniciando Sesion ..............");
+    }
+    public static int busqueda(int[] direccion, int busc){
+        for (int x=0; x<direccion.length;x++){
+            if(direccion[x]==busc){
+                return x;
+            }
+        }
+        return -1;
     }
     public static void wait(int segundos){
         try {
@@ -32,27 +59,27 @@ public class billeteraDigital {
             System.out.println(e);
          }
     }
-    public static void menu(){
-        int opc, id2, monto, z, compra, i, x=0, a=1, opc2;
+    public static void menu(int episicoin){
+        int opc,id2,ns,monto, z, compra, i, x=0, a=1, opc2;
         /*episicoin=episicoin-monto+compra/100;*/
-	/*ns=episicoin*100;*/
-        /*System.out.println("          ID:" + id);
+	    ns=episicoin*100;
+        /*System.out.println("          ID:" + id);*/
         System.out.println("|========= " + episicoin + " EPISIcoin =========|");
-        System.out.println(" ----------- S/." + ns + " -------------");*/
+        System.out.println(" ----------- S/." + ns + " -------------");
         System.out.print("  " + "1.Enviar" + "     ");
-	System.out.print("2.Comprar" + "     ");
+	    System.out.print("2.Comprar" + "     ");
         System.out.println("3.Activos" + "     ");
         System.out.print("4.Actividad" + "     ");
         System.out.print("5.Otro" + "     ");
-	System.out.println("6.Cerrar Sesion");
+	    System.out.println("6.Cerrar Sesion");
         System.out.println("Seleccionar:");
         Scanner teclado = new Scanner(System.in);
-	opc=teclado.nextInt();
-        switch (opc) {
+	    opc=teclado.nextInt();
+        switch(opc) {
             case 1:
                 System.out.println("------------- Enviar a ------------");
                 System.out.print("introduce la direccion:");
-	        id2=teclado.nextInt();
+	            id2=teclado.nextInt();
 	        /*wait(2)*/
                 System.out.println("Buscando....");
                 /*wait(2)*/
@@ -121,7 +148,6 @@ public class billeteraDigital {
                 opc2=teclado.nextInt();
                 switch (opc2) {
                     case 1: /*wait*/
-                        
                         break;
                     case 2: System.out.println("Agregar activo");
                             //case 3
@@ -129,11 +155,9 @@ public class billeteraDigital {
                     default:
                         throw new AssertionError();
                 }
-		   //menu
-  /* menu(id, episicoin, monto, ns, compra)*/
                 break;    
             default:
-                System.out.println("Selecciona la opcion correcta");;
+                System.out.println("Selecciona la opcion correcta");
         } while(opc != 0);
         System.out.println("Se termino el programa........");
     }
